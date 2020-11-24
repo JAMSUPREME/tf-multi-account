5f9725d96ed0e7481d5395c7331b78c977eacec0
Justin Spencer <justin.spencer@icf.com>
2020-11-24 13:52:39 -0500

    promotion codebuild trigger

--- terraform/application/cloudwatch_receive_promotion.tf
+++ terraform/application/cloudwatch_receive_promotion.tf
@@ -23,3 +23,57 @@ resource "aws_cloudwatch_event_target" "receive_promotion_target_email" {
     }
   )
 }
+
+resource "aws_cloudwatch_event_target" "receive_promotion_target_codebuild" {
+  count     = var.lower_environment_account_number == "" ? 0 : 1
+  rule      = aws_cloudwatch_event_rule.receive_promotion[0].name
+  target_id = "TriggerCodeBuild"
+  arn       = aws_codebuild_project.docker_builder.arn
+  role_arn  = aws_iam_role.cloudwatch_to_codebuild.arn
+}
+
+#
+# IAM
+#
+resource "aws_iam_role" "cloudwatch_to_codebuild" {
+  name = "${var.deploy_env}-cloudwatch-to-codebuild"
+
+  assume_role_policy = <<-POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "events.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  POLICY
+
+  tags = local.global_tags
+}
+
+resource "aws_iam_role_policy" "cloudwatch_to_codebuild_policy" {
+  name = "${var.deploy_env}-cloudwatch-to-codebuild-policy"
+  role = aws_iam_role.cloudwatch_to_codebuild.name
+
+  policy = <<-POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Resource": [
+          "${aws_codebuild_project.docker_builder.arn}"
+        ],
+        "Action": [
+          "codebuild:StartBuild"
+        ]
+      }
+    ]
+  }
+  POLICY
+}
